ff59525157a5fa5e2206702f6ed9fb01e7d62681
lnddandekar <50483930+lnddandekar@users.noreply.github.com>
2023-06-14 17:46:57 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload(){
+src="https://editor.p5js.org/WhithatJr/sketches/5t1M9o4eX"
+}
+function setup(){
+    canvas=createCanvas(640,480);
+    canvas.position(110,250);
+    video=createCapture(VIDEO);
+    video.hide();
+    tint_color="";
+}
+function draw(){
+    image(video,0,0,640,480);
+    tint(tint_colour);
+}
+function take_snapshot(){
+    save('student_name.png');
+}
+function filter_tint()
+{
+    tint_color=document.getElementById("color_name").value;
+}
